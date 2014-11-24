d677d4fc0c773a7caabf4fa5a6053adc1a2da9ac
Emilie <emilie@saisie.org>
2014-11-24 22:01:11 +0100

    Add stream

--- app.rb
+++ app.rb
@@ -1,6 +1,27 @@
 require 'sinatra'
 require 'slim'
+require 'better_errors'
+require 'coffee_script'
+
+configure :development do
+  use BetterErrors::Middleware
+  BetterErrors.application_root = __dir__
+end
+
+set :server, :thin
+connections = []
 
 get '/' do
-  slim :index
+  slim :home
+end
+
+get '/stream', provides: 'text/event-stream' do
+  stream :keep_open do |out|
+    EventMachine::PeriodicTimer.new(3) do
+      now = Time.now
+      if now >= Time.new(now.year, now.month, now.day, 21, 57) && now <= Time.new(Time.now.year, Time.now.month, Time.now.day, 21, 58)
+        out << "data: #{Time.now}\n\n"
+      end
+    end
+  end
 end
